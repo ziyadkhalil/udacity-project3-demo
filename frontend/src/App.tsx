cb7922ef03836d389f9f7bfb2bca50d3e8c600d8
Ziyad Khalil <ziyadhkhalil@gmail.com>
2022-08-09 22:20:01 +0200

    elastic beanstalk region

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+const fetchData = async () =>
+  (await axios.get(process.env.REACT_APP_API_URL!)).data.data;
+
 function App() {
   const [backendResponse, setbackendResponse] = useState();
   useEffect(() => {
-    axios
-      .get(process.env.REACT_APP_API_URL!)
-      .then((res) => setbackendResponse(res.data.data));
+    fetchData().then(setbackendResponse);
   }, []);
   return <div>Backend Response is: {backendResponse}</div>;
 }
